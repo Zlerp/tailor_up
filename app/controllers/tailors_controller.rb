fbb6bbd04259b772bbaf51705a02ada0021f1035
Lenora Porter <lporter630@gmail.com>
2015-08-01 16:27:50 -0400

    Create Tailor successfully

--- app/controllers/tailors_controller.rb
+++ app/controllers/tailors_controller.rb
@@ -4,5 +4,22 @@ class TailorsController < ApplicationController
   @tailor = Tailor.new
   end
 
-  
+  def create
+  @tailor = Tailor.new tailor_params
+    if @tailor.save
+    redirect_to root_path, notice: 'Tailor: #{@tailor.first_name} #{@tailor.last_name} has been created!'
+    else
+    render :new
+  end
+end
+private
+
+def set_tailor
+  @tailor = Tailor.find(params[:id])
+end
+
+def tailor_params
+  params.require(:tailor).permit(:first_name, :last_name, :email, :password, :password_confirmation, :location, :phone, :avatar)
+end
+
 end
