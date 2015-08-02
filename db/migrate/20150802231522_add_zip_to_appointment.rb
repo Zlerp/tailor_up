0742c1f21573a93686f05f35358ef0b8d70ce07a
Zlerp <zhpdeals@gmail.com>
2015-08-02 19:21:13 -0400

    Add Appointments Zipcode Validation

---
+++ db/migrate/20150802231522_add_zip_to_appointment.rb
@@ -0,0 +1,5 @@
+class AddZipToAppointment < ActiveRecord::Migration
+  def change
+    add_column :appointments, :zip, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150802221648) do
+ActiveRecord::Schema.define(version: 20150802231522) do
 
   create_table "appointments", force: :cascade do |t|
     t.string   "stages"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150802221648) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "user_id"
+    t.string   "zip"
   end
 
   create_table "articles", force: :cascade do |t|
