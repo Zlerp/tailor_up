768b044670c467f6f6dfe86732b08f1df1b977f3
Andre <afre92@gmail.com>
2015-08-04 18:19:17 -0400

    appointment can be created again

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  before_action :set_appointment, only: [:create]
+  before_action :set_appointment, only: [:index, :create]
   # GET /articles
   # GET /articles.json
   def index
