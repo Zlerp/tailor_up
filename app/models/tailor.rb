1a602c9584251b57cb330da96c3319cd4b312496
Zlerp <zhpdeals@gmail.com>
2015-08-10 21:06:29 -0400

    email and name casing

--- app/models/tailor.rb
+++ app/models/tailor.rb
@@ -7,12 +7,18 @@ class Tailor < ActiveRecord::Base
 
   belongs_to :company
   mount_uploader :image, ImageUploader
+  before_save :format_values
   # validates_uniqueness_of :email, :phone
   # validates :first_name, presence: true
   # validates :last_name, presence: true
   # validates_format_of :phone, :with => /\A(1?(?: |\-|\.)?(?:\(\d{3}\)|\d{3})(?: |\-|\.)?\d{3}(?: |\-|\.)?\d{4})\z/, message: "incorrect format, ex. 888-888-8888"
 
 
+  def format_values
+    first_name.capitalize!
+    last_name.capitalize!
+    email.donwcase!
+  end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :appointments
   mount_uploader :image, ImageUploader
+
+  before_save :format_values
   # validates :first_name, presence: true
   # validates :last_name, presence: true
   # validates_uniqueness_of :email, :phone
@@ -12,5 +14,9 @@ class User < ActiveRecord::Base
   ########## For Faker Gem ONLY  ############
   # validates_uniqueness_of :email, :phone, :first_name, :last_name, :address
 
-
+  def format_values
+    first_name.capitalize!
+    last_name.capitalize!
+    email.donwcase!
+  end
 end
