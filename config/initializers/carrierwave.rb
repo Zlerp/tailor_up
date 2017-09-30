ad0218a210238fed2ee205e6b7d8b27bf8937532
Andre <afre92@gmail.com>
2017-09-30 12:47:35 -0400

    background images for company and tailor updated

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     ENV['AWSAccessKeyId'],
-    aws_secret_access_key: ENV['AWSSecretKey'],
+    aws_access_key_id:     'nothinghere', #env values remove since I dont have them
+    aws_secret_access_key: 'nothinhereeaither',
     }
   config.fog_directory  = 'tailor'
 end
